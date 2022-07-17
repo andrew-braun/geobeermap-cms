6109feaeb6c3f5f19382663d058c0bc3ed047471
Andrew Braun <andrew@andrewbraun.dev>
2022-07-17 15:15:14 +0400

    feat: Add custom controller to venue to populate relations and limit returned fields

--- src/api/venue/controllers/venue.js
+++ src/api/venue/controllers/venue.js
@@ -1,9 +1,65 @@
-'use strict';
+"use strict";
 
 /**
  *  venue controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::venue.venue');
+// module.exports = createCoreController('api::venue.venue');
+
+const uid = "api::venue.venue";
+
+module.exports = createCoreController(uid, () => {
+  const components = {
+    location: {
+      populate: {
+        city: {
+          fields: ["name", "slug", "locale"],
+        },
+        neighborhood: {
+          fields: ["name", "slug", "locale"],
+        },
+      },
+    },
+    business_information: {
+      populate: {
+        logo: {
+          fields: ["name", "alternativeText", "width", "height", "url"],
+        },
+      },
+    },
+  };
+
+  return {
+    async find(ctx) {
+      // overwrite default populate=* functionality
+      if (ctx.query.populate === "*") {
+        const entity = await strapi.entityService.findMany(uid, {
+          ...ctx.query,
+          populate: components,
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+      }
+      // maintain default functionality for all other request
+      return super.find(ctx);
+    },
+    async findOne(ctx) {
+      const { id } = ctx.request.params;
+
+      if (ctx.query.populate === "*") {
+        const entity = await strapi.entityService.findOne(uid, id, {
+          ...ctx.query,
+          populate: components,
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+      }
+
+      return super.findOne(ctx);
+    },
+  };
+});
