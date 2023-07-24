4d70baa328b73168b27e89bba776d527cc2e2fbf
andrew-braun <andrew@andrewbraun.dev>
2023-07-24 18:51:46 +0400

    test: Test railway deploys

--- src/api/venue/routes/custom-venue.js
+++ src/api/venue/routes/custom-venue.js
@@ -1,7 +1,7 @@
 module.exports = {
     routes: [
         {
-            // This route can probably be deleted later
+            // This route can probably be deleted later (test)
             method: "POST",
             path: "/venue/netlify-json",
             handler: "venue.processNetlifyJSON",
