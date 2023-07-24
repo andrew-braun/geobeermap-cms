9c3fcbfd5a43d2877f850bc45ca78e0b46e9f2aa
andrew-braun <andrew@andrewbraun.dev>
2023-07-24 18:48:21 +0400

    chore: Add comment for future deletion

--- src/api/venue/routes/custom-venue.js
+++ src/api/venue/routes/custom-venue.js
@@ -1,6 +1,7 @@
 module.exports = {
     routes: [
         {
+            // This route can probably be deleted later
             method: "POST",
             path: "/venue/netlify-json",
             handler: "venue.processNetlifyJSON",
