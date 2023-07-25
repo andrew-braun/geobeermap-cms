544dc43b1a68c5f5a231a94a043079a23a107b80
andrew-braun <andrew@andrewbraun.dev>
2023-07-25 12:51:02 +0400

    feat: Switch CLOUDINARY_API_KEY to CLOUDINARY_KEY to sync with Railway .env

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       provider: "cloudinary",
       providerOptions: {
         cloud_name: process.env.CLOUDINARY_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
+        api_key: process.env.CLOUDINARY_KEY,
         api_secret: process.env.CLOUDINARY_API_SECRET,
       },
       actionOptions: {
